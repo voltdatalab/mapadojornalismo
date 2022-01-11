3c2e24395932de27e64a1511581a5f81e847ecee
sspagnuolo <sergio@voltdata.info>
2022-01-11 19:10:23 -0300

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+(function () {
+
+    'use strict';
+
+    var app = angular.module(
+        'app',
+        []
+    );
+
+    app.run(['$window', '$rootScope', '$timeout', '$location', '$http', function ($window, $rootScope, $timeout, $location, $http) {
+
+
+    }]);
+
+})();
