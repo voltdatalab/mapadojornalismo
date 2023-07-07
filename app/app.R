e2c9c4cdf501212e0ae1b91e24dac487b5279030
sspagnuolo <info@voltdata.info>
2023-07-07 15:46:32 -0300

    fix mapa

--- app/app.R
+++ app/app.R
@@ -3,34 +3,40 @@ library(dplyr)
 library(htmlwidgets)
 library(shinyWidgets)
 library(stringr)
+library(readr)
+
+options(scipen=999)
 
   server <- function(input, output, session){
     
     dados <- reactive({
       
-      # main_table <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTZkC54Oei0Ab_YUbcayci0IHd3TMj5qKcnYFARoyx5l2hpFZRlO1TEV6diR3HE22gm2QT89z4itj2C/pub?gid=993866458&single=true&output=csv", header = T)
+      #antiga https://docs.google.com/spreadsheets/d/e/2PACX-1vTZkC54Oei0Ab_YUbcayci0IHd3TMj5qKcnYFARoyx5l2hpFZRlO1TEV6diR3HE22gm2QT89z4itj2C/pub?gid=993866458&single=true&output=csv
+      main_table <- read_csv("https://noco.db.enoisconteudo.com.br/api/v1/db/public/shared-view/6770570c-860c-4c7d-b6bf-41130dfecf4e/rows/export/csv", 
+                             col_types = cols(Lat = col_number(), 
+                                              Long = col_number()))
       
-      if (input$cobre_cultura == "TRUE") {
+      if (input$cultura == "TRUE") {
       main_table <- main_table %>%
-        filter(cobre_cultura == "sim")
+        filter(CobreCultura == "sim")
       }
       
       if (input$nome != "") {
         main_table <- main_table %>%
-          filter(str_detect(nome_veiculo, regex(input$nome, ignore_case = T)))
+          filter(str_detect(NomeVeiculo, regex(input$nome, ignore_case = T)))
       }
       
       if (input$areas != "Todos") {
-        main_table <- main_table[main_table$principal_cobertura == input$areas,]
+        main_table <- main_table[main_table$PrincipalCobertura == input$areas,]
       }
       
-      if (input$localizacao != "Todas") {
-        main_table <- main_table[main_table$localizacao == input$localizacao,]
+      if (input$cidade != "Todas") {
+        main_table <- main_table[main_table$Cidade == input$cidade,]
       }
       
-      main_table <- main_table %>%
-        filter(formato == input$formato[1] | formato == input$formato[2] | formato == input$formato[3])
-      
+      # main_table <- main_table %>%
+      #   filter(Formato == input$formato[1] | Formato == input$formato[2] | Formato == input$formato[3])
+
       # if (input$raca_responsavel != "Mostrar tudo") {
       #   main_table <- main_table[main_table$raca_responsavel == input$raca_responsavel,]
       # }
@@ -60,23 +66,23 @@ library(stringr)
       dados <- dados()
       
       content <- paste("<div class='popup_format'>
-                       <h3>", dados$nome_veiculo, "</h3>", 
-                       "<h4 class='subtitle'><strong>Localidade:</strong>", dados$localizacao, "</h4>",
-                       "<span>",dados$formato, "</span><br>",
-                       "<h4><strong>Segmentos de atuação</strong>:", dados$segmento_de_atuacao, "</h4>",
-                       "<h4><strong>Principal cobertura</strong>:", dados$principal_cobertura, "</h4>",
-                       "<h4><strong>Cobre Cultura</strong>:", dados$cobre_cultura, "</h4>",
-                       if(dados$cobre_cultura == "sim")
-                         {paste("<h4><strong>Tipo de cobertura cultural</strong>:", dados$topicos_cultura, "</h4>")},
-                       if(dados$qtd_colaboradores != "S/I")
-                       {paste("<h4><strong>Tamanho</strong>:", dados$qtd_colaboradores, "</h4>")},
-                       "<h4><strong>Modelo de negócios</strong>:", dados$modelo_negocios, "</h4>",
-                       "<h4><a href='", dados$link_social, "' target='_blank' style='color:#8368E0;font-weight:700'> &#9758; Saiba mais </a></h4></div>"
+                       <h3>", dados$NomeVeiculo, "</h3>", 
+                       "<h4 class='subtitle'><strong>Localidade:</strong>", dados$Cidade, "</h4>",
+                       "<span>",dados$Formato, "</span><br>",
+                       "<h4><strong>Segmentos de atuação</strong>:", dados$SegmentoDeAtuacao, "</h4>",
+                       "<h4><strong>Principal cobertura</strong>:", dados$PrincipalCobertura, "</h4>",
+                       "<h4><strong>Cobre Cultura</strong>:", dados$CobreCultura, "</h4>",
+                       #if(dados$CobreCultura == "sim")
+                      #   {paste("<h4><strong>Tipo de cobertura cultural</strong>:", dados$topicos_cultura, "</h4>")},
+                       #if(dados$qtd_colaboradores != "S/I")
+                       #{paste("<h4><strong>Tamanho</strong>:", dados$qtd_colaboradores, "</h4>")},
+                       "<h4><strong>Modelo de negócios</strong>:", dados$ModeloNegocios, "</h4>",
+                       "<h4><a href='", dados$LinkSocial, "' target='_blank' style='color:#8368E0;font-weight:700'> &#9758; Saiba mais </a></h4></div>"
       )
       
-      initial_lat = -23.65577
-      initial_lng = -46.53956
-      initial_zoom = 10
+      initial_lat = -28.65577
+      initial_lng = -60.53956
+      initial_zoom = 4
       
       icons <- awesomeIcons(
         icon = 'newspaper',
@@ -90,8 +96,9 @@ library(stringr)
         leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
         setView(lat = initial_lat, lng = initial_lng, zoom = initial_zoom) %>%
         addProviderTiles(providers$CartoDB.Positron) %>%
+        #addMarkers(clusterOptions = markerClusterOptions())
         addAwesomeMarkers(
-          clusterOptions = markerClusterOptions(), label = dados$nome_veiculo, popup = content, icon=icons
+          clusterOptions = markerClusterOptions(), label = dados$NomeVeiculo, popup = content, icon=icons
         )
       
       mt
@@ -115,9 +122,9 @@ library(stringr)
       tags$div(class="explain",tags$p(tags$b("Mapeamento busca veículos de comunicação periféricos em diversas cidades de São Paulo para entender a cobertura local")),
       tags$div(class="filtros_main", 
                switchInput(
-                 inputId = "cobre_cultura",
+                 inputId = "cultura",
                  label = tags$span( icon("hand-pointer"),"Mudar cobertura"),
-                 value = TRUE,
+                 value = FALSE,
                  onLabel = "Apenas cultura",
                  offLabel = "Todas as áreas",
                  onStatus = TRUE,
@@ -133,7 +140,7 @@ library(stringr)
                ),
       tags$p(tags$b(textOutput("n_veiculos"))),
       tags$div(class="filtros_main", radioButtons(inputId = "checkbox", label = "FILTROS DE PESQUISA", choices = c("Mostrar", "Esconder"), inline = TRUE, selected = "Esconder")),
-      conditionalPanel(condition="input.cobre_cultura=='FALSE'",
+      conditionalPanel(condition="input.CobreCultura=='FALSE'",
       tags$p("Hoje, temos 21 milhões de habitantes nas 40 cidades que compõem São Paulo e Região Metropolitana. Grande parte dessa população é migrante e trouxe consigo hábitos e costumes locais pouco divulgados e valorizados nos territórios. Queremos identificar iniciativas para compreender como as periferias dessas regiões se reconhecem e o que sabem e divulgam sobre a informação e a produção cultural local.")))
     ),
     conditionalPanel(condition="input.checkbox=='Mostrar'",
@@ -147,7 +154,7 @@ library(stringr)
       #left = 300,
       #right = "auto",
       #bottom = "auto",
-      width = 650,
+      width = 550,
       height = "auto",
       tags$h5("FILTROS"),
       column(12, 
@@ -179,65 +186,96 @@ library(stringr)
              ),
       column(12,
       column(6,
-             selectizeInput(inputId = "localizacao", 
+             selectizeInput(inputId = "cidade", 
                             #multiple = TRUE,
                             label = 'Escolha a cidade',
-                            choices  = c('Todas', 'Arujá',
-                                         'Barueri',
-                                         'Biritiba Mirim',
-                                         'Caieiras',
-                                         'Cajamar',
-                                         'Carapicuíba',
-                                         'Cotia',
-                                         'Diadema',
-                                         'Embu das Artes',
-                                         'Embu-Guaçu',
-                                         'Ferraz de Vasconcelos',
-                                         'Francisco Morato',
-                                         'Franco da Rocha',
-                                         'Guararema',
-                                         'Guarulhos',
-                                         'Itapecerica da Serra',
-                                         'Itapevi',
-                                         'Itaquaquecetuba',
-                                         'Jandira',
-                                         'Juquitiba',
-                                         'Mairiporã',
-                                         'Mauá',
-                                         'Mogi das Cruzes',
-                                         'Osasco',
-                                         'Pirapora do Bom Jesus',
-                                         'Poá',
-                                         'Ribeirão Pires',
-                                         'Rio Grande da Serra',
-                                         'Salesópolis',
-                                         'Santa Isabel',
-                                         'Santana de Parnaíba',
-                                         'Santo André',
-                                         'São Bernardo do Campo',
-                                         'São Caetano do Sul',
-                                         'São Lourenço da Serra',
-                                         'São Paulo',
-                                         'Suzano',
-                                         'Taboão da Serra',
-                                         'Vargem Grande Paulista'),
+                            choices  = c('Todas', 
+                                         'Abadia de Goiás ',
+                                         'Acorizal',
+                                         'Alto Alegre',
+                                         'Alto Paraguai',
+                                         'Ananindeua ',
+                                         'Aparecida de Goiânia ',
+                                         'Aparecida do Rio Negro',
+                                         'Araguaína',
+                                         'Araguantins',
+                                         'Barra do Bugres',
+                                         'Barrolândia',
+                                         'Bela Vista de Goiás',
+                                         'Belém',
+                                         'Benevides',
+                                         'Boa Vista',
+                                         'Bonfinópolis ',
+                                         'Cáceres',
+                                         'Campo Grande',
+                                         'Candeias do Jamari',
+                                         'Cantá',
+                                         'Careiro da Várzea',
+                                         'Chapada dos Guimarães',
+                                         'Cuiabá',
+                                         'Distrito Federal',
+                                         'Divinópolis do Tocantins',
+                                         'Fátima',
+                                         'Goânia',
+                                         'Goianira',
+                                         'Goiás Velho ',
+                                         'Guapó ',
+                                         'Gurupi',
+                                         'Inhumas',
+                                         'Iranduba',
+                                         'Itacoatiara ',
+                                         'Lajeado',
+                                         'Macapá',
+                                         'Manacapuru',
+                                         'Manaus',
+                                         'Marituba',
+                                         'Mazagão',
+                                         'Miracema',
+                                         'Miranorte',
+                                         'Mucajaí',
+                                         'Muricilândia',
+                                         'Nerópolis',
+                                         'Nobres ',
+                                         'Nossa Senhora do Livramento',
+                                         'Nova Gama',
+                                         'Nova Veneza',
+                                         'Novo Airão',
+                                         'Pacaraima',
+                                         'Palmas',
+                                         'Paraíso do Tocantins',
+                                         'Paranatinga',
+                                         'Porto Espiridião',
+                                         'Porto Nacional',
+                                         'Porto Velho',
+                                         'Presidente Figueiredo',
+                                         'Rio Branco',
+                                         'Rio Preto da Eva',
+                                         'Santa Izabel ',
+                                         'Santana',
+                                         'Senador Canedo',
+                                         'Taipas do Tocantins',
+                                         'Tangará da Serra',
+                                         'Trindade ',
+                                         'Valparaíso de Goiás',
+                                         'Várzea Grande',
+                                         'Vila Bela da Santíssima Trindade '),
                             selected = "Todas")),
-      column(6,checkboxGroupInput(inputId = "formato",
-                                  label = "Segmento",
-                                  #multiple = TRUE,
-                                  choices = c("Impresso",
-                                              "Online",
-                                              "Rádio",
-                                              "TV"),
-                                  selected = c("Impresso",
-                                               "Online",
-                                               "Rádio",
-                                               "TV"),
-                                  inline = TRUE
-      ))
+      # column(6,checkboxGroupInput(inputId = "formato",
+      #                             label = "Segmento",
+      #                             #multiple = TRUE,
+      #                             choices = c("Impresso",
+      #                                         "Online",
+      #                                         "Rádio",
+      #                                         "TV"),
+      #                             selected = c("Impresso",
+      #                                          "Online",
+      #                                          "Rádio",
+      #                                          "TV"),
+      #                             inline = TRUE
+      # ))
       ),
       # column(12,
-      # # column(4,radioButtons(inputId = "cobre_cultura",
+      # # column(4,radioButtons(inputId = "CobreCultura",
       # #                             label = "Cobertura cultura",
       # #                             #multiple = TRUE,
       # #                             choices = c("Mostrar tudo",
